1175ca35630f45a6900a2fa3116a2c090e10a071
Pablo-githubb <pablomaso@iesebre.com>
2024-11-11 23:47:26 +0100

    matrius

--- MyFirstRepository/Activatat1Java/src/main/java/expliacions/introduccio/Enunciats/exercicis/precedència/Dilluns20241111.java
+++ MyFirstRepository/Activatat1Java/src/main/java/expliacions/introduccio/Enunciats/exercicis/precedència/Dilluns20241111.java
@@ -35,19 +35,68 @@ class indexofmatriu {
 
 
             for (int i = 0; i < files*columnes; i++) {
-                matriu[i]= ent.nextInt();
+             //
             }
 
             int posiciofila = 0;
             int posiciocolumna = 0;
             int num = ent.nextInt();
             for (int i = 0; i < files*columnes; i++) {
-                    if (matriu[i]==num) {
+                   // if (matriu[i]==num) {
                         posiciofila = i;
                     }
                 }
-                System.out.format("%d %d\n", posiciofila, posiciocolumna);
+               // System.out.format("%d %d\n", posiciofila, posiciocolumna);
             }
         }
-    }
 
+class sumafilescolumnes {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+
+        int files = ent.nextInt();
+        int columnes = ent.nextInt();
+        int[][] matriu = new int[files][columnes];
+
+        for (int i = 0; i < files; i++) {
+            for (int j = 0; j < columnes; j++) {
+                matriu[i][j] = ent.nextInt();
+            }
+        }
+        int k = ent.nextInt();
+        int sumafila = 0;
+        int sumacolumna = 0;
+        for (int i = 0; i < files; i++) {
+            sumafila += matriu[i][k];
+        }
+
+        for (int j = 0; j < columnes; j++) {
+            sumacolumna += matriu[k][j];
+        }
+        System.out.format("%d %d ", sumacolumna, sumafila);
+    }
+}
+
+class matriuidentitat{
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+
+        int k = ent.nextInt();
+        int[][] matriu = new int[k][0];
+
+        matriu[0] = new int[5];
+        matriu[1] = new int[5];
+        matriu[2] = new int[5];
+        matriu[3] = new int[5];
+        matriu[4] = new int[5];
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                matriu[i][j]=0;
+                System.out.format("5%d ", matriu[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
+//se perfectament que no esta be. Pero, no se com acabar-ho

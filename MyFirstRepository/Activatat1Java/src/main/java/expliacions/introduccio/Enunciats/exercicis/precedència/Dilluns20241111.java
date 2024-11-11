d83fdafb1349b25603ae15eb8dea9676a01d0fdc
Pablo-githubb <pablomaso@iesebre.com>
2024-11-11 23:56:37 +0100

    matrius

--- MyFirstRepository/Activatat1Java/src/main/java/expliacions/introduccio/Enunciats/exercicis/precedència/Dilluns20241111.java
+++ MyFirstRepository/Activatat1Java/src/main/java/expliacions/introduccio/Enunciats/exercicis/precedència/Dilluns20241111.java
@@ -6,18 +6,28 @@ import java.util.Scanner;
 public class Dilluns20241111 {
     public static void main(String[] args) {
         Scanner ent = new Scanner(System.in);
+        //Declaració + instanciació d'un array unidimensional
+        int[] diesMesosAny={31,28,31,30,31,30,31,31,30,31,30,31};   //sense new només la moment de declarar
+        int[] copia=diesMesosAny;
 
-        int[] diesMesosAny={31,28,31,30,31,31,31,31,30,31,30,31};
-        diesMesosAny=new int[]{31,28,31,30,31,31,31,31,30,31,30,31};
+        copia[1]=29;
+        System.out.println(diesMesosAny[1]);        //vorem un 29
+
+        //diesMesosAny={31,28,31,30,31,30,31,31,30,31,30,31}; --> no es pot fer
+        diesMesosAny=new int[]{31,31,30,31};    //en new ho podem fer en qualsevol moment
+        System.out.println(diesMesosAny[1]);        //vorem un 31
 
 
-        int[][] diesMesosAnyBi={{31},{31,28,31,30,31,31,31,31,30,31,30,31}};
-        diesMesosAnyBi= new int[][]{{31},{30,31,30,31}};
+        //Declaració + instanciació d'un array bidimensional
+        int[][] diesMesosAnyBi={{31},{28,31,30,31,30,31,31,30,31,30,31}};   //sense new només la moment de declarar
+        //diesMesosAnyBi={{31},{28,31,30,31,30,31,31,30,31,30,31}};
+        diesMesosAnyBi=new int[][]{{31},{30,31,30,31}};
 
-        int[][][][][] cosa=new int[5][][][][];
-
-
-
+        //Arrays multidimensionals
+        int[][][][][][] cosa=new int[5][][][][][];
+        //int[][][][][][] cosa=new int[5][][][3][][]; __> no podem tindre dimensions buides per mig de plenes
+        int[][][][][][] cosa2=new int[5][3][4][][][];
+        cosa[0]=new int[3][][][][];
 
     }
 }

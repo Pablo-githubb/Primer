f77089ae496c173862dd3615202912b18883f1f0
Pablo-githubb <pablomaso@iesebre.com>
2024-11-22 13:33:32 +0100

    problemes

--- MyFirstRepository/Activatat1Java/src/main/java/expliacions/introduccio/Enunciats/exercicis/precedència/Divendres1122.java
+++ MyFirstRepository/Activatat1Java/src/main/java/expliacions/introduccio/Enunciats/exercicis/precedència/Divendres1122.java
@@ -1,5 +1,6 @@
 package expliacions.introduccio.Enunciats.exercicis.precedència;
 
+import java.security.spec.RSAPrivateCrtKeySpec;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -23,7 +24,7 @@ public class Divendres1122 {
                 //Tractament de la casella actual
                 int suma = 0;
                 //Caselles exteriors
-                if (i == 1 || i == m.length +1|| j == 1 || j == m[i].length+1) {
+                if (i == 0 || i == m.length - 1 || j == 0 || j == m[i].length - 1) {
                     if (i == 0) suma = m[i][j - 1] + m[i][j + 1] + m[i + 1][j - 1] + m[i + 1][j] + m[i + 1][j + 1];
 
                 }
@@ -38,3 +39,54 @@ public class Divendres1122 {
     }
 }
 
+class desierto {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+
+        int numCasos = ent.nextInt();
+
+        while (numCasos-- > 0) {
+            //tractament de cada cas
+            int numMapes = ent.nextInt(); //numero de mapes a llegir, a l'enunciat
+
+
+            BattleField[] mapes = new BattleField[numMapes];
+            while (numMapes-- > 0) {
+
+                String nomMapa = ent.skip("[\r\n]*").nextLine();
+                //Anem a buscar el nom del mapa al vetor
+
+                int i = 0; //Declarem el index fora per poder utilitzar en el if de fora
+                for (; i < mapes.length && mapes[i] != null && !mapes[i].nom.equals(nomMapa); i++) {
+                    if (mapes[i] != null) {
+                        BattleField mapa = new BattleField();
+                        mapa.nom = nomMapa;
+                        mapa.numVots = 1; //el primer vot que rep aquest mapa
+                        mapes[i] = mapa;
+                    } else {
+                        mapes[i].numVots++;
+                    }
+                }
+                //Anem a bucar al guanyador
+
+                int maximvots = 0, indexMaxim=-1;
+                for (; i < mapes.length && mapes[i]!=null; i++) {
+                    if (mapes[i].numVots>maximvots) {
+                        maximvots=mapes[i].numVots;
+                        indexMaxim=i;
+                    }
+                }
+                //El resultat sera
+                System.out.println(mapes[indexMaxim].nom);
+            }
+        }
+    }
+}
+
+class BattleField {
+    //Propietats de la clase
+
+    String nom;
+    int numVots = 0;
+
+}

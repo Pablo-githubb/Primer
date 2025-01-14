2a25db6fd8abb75df9b5eef7537d124aa62fb270
Pablo-githubb <pablomaso@iesebre.com>
2025-01-14 11:41:35 +0100

    data.java

--- UF2/UsingMyFirstLibrary/src/main/java/org/example/Data.java
+++ UF2/UsingMyFirstLibrary/src/main/java/org/example/Data.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import static com.iesebrepablo.library20242024.myfirstlibrary.Data.*;
+
 public class Data {
+    public static void main(String[] args) {
+        System.out.println(esBixest(1));
+        System.out.println(dataCorrecta(31,78,2000));
+        System.out.println(diaSeguent(31,12,2016));
+        System.out.println(comparaDates(1,12,2000,31,11,2011));
+        System.out.println(diferenciaDies(11,6,2006,27,11,2007));
+    }
+
 }

c474bde667e6595286148e9e21eb6814bebcb1e6
Pablo-githubb <pablomaso@iesebre.com>
2025-01-14 11:41:04 +0100

    update

--- UF2/MyFirstLibrary/src/main/java/com/iesebrepablo/library20242024/myfirstlibrary/Data.java
+++ UF2/MyFirstLibrary/src/main/java/com/iesebrepablo/library20242024/myfirstlibrary/Data.java
@@ -51,24 +51,86 @@ public class Data {
     }
 
     /**
-     * Compara dues dates donades per dia, mes i any.
+     * Diu si les dates que rep són diferents o iguals
      *
-     * @param dia1 el dia de la primera data
-     * @param mes1 el mes de la primera data
-     * @param any1 l'any de la primera data
-     * @param dia2 el dia de la segona data
-     * @param mes2 el mes de la segona data
-     * @param any2 l'any de la segona data
-     * @return 1 si la primera data és major que la segona, 0 si són iguals, -1 si la primera és menor que la segona, i -2 si alguna de les dates no és correcta
+     * @param dia1 un número que representa el dia de la primera data
+     * @param mes1 un número que representa el mes de la primera data
+     * @param any1 un número que representa l'any de la primera data
+     * @param dia2 un número que representa el dia de la segona data
+     * @param mes2 un número que representa el mes de la segona data
+     * @param any2 un número que representa l'any de la segona data
+     * @return 1 si la primera data és major que la segona, 0 si són iguals, -1 si és menor, i -2 si alguna data és incorrecta
      */
     public static int comparaDates(int dia1, int mes1, int any1, int dia2, int mes2, int any2) {
-
+        //Primer comprovem si la dates són correctes
         if (!dataCorrecta(dia1, mes1, any1) || !dataCorrecta(dia2, mes2, any2)) return -2;
-        if (any1 > any2 || (any1 == any2 && mes1 > mes2) || (any1 == any2 && mes1 == mes2 && dia1 > dia2)) return 1;
-        else if (any1 == any2 && mes1 == mes2 && dia1 == dia2) return 0;
-        else return -1;
-        }
+
+        //primer comparem els anys
+        if (any1 > any2) return 1;
+        else if (any1 < any2) return -1;
+
+        //Aquí arribem si els anys són iguals, per tant ara comparem els mesos
+        if (mes1 > mes2) return 1;
+        else if (mes1 < mes2) return -1;
+
+        //Aquí arribem si els mesos també són iguals, per tant ara comparem els dies
+        if (dia1 > dia2) return 1;
+        else if (dia1 < dia2) return -1;
+
+        //Aquí arribem si les 2 dates eren idèntiques
+        return 0;
     }
+
+    /**
+     * Diu quants dies hi ha entre 2 dates
+     *
+     * @param dia1 un número que representa el dia de la primera data
+     * @param mes1 un número que representa el mes de la primera data
+     * @param any1 un número que representa l'any de la primera data
+     * @param dia2 un número que representa el dia de la segona data
+     * @param mes2 un número que representa el mes de la segona data
+     * @param any2 un número que representa l'any de la segona data
+     * @return El número de dies entre les dates si estes són correctes, i -1 si hi ha alguna data incorrecta
+     */
+    public static int diferenciaDies(int dia1, int mes1, int any1, int dia2, int mes2, int any2) {
+        //Primer comprovem si la dates són correctes
+        if (!dataCorrecta(dia1, mes1, any1) || !dataCorrecta(dia2, mes2, any2)) return -1;
+
+        //Ara comprovem quina data és major i, si cal, fem que la primera sigue menor que la segona
+        switch (comparaDates(dia1, mes1, any1, dia2, mes2, any2)) {
+            case 0:
+                //En este cas les 2 dates eren la mateixa, per tant acabem retornant 0
+                return 0;
+            case 1:
+                //En este cas la primera data és major que la segona, per tant les intercanviem
+                int temp = dia1;
+                dia1 = dia2;
+                dia2 = temp;
+                temp = mes1;
+                mes1 = mes2;
+                mes2 = temp;
+                temp = any1;
+                any1 = any2;
+                any2 = temp;
+        }
+
+        //Si arribem aquí segur que la primera data és  menor que la segoan, per tant comptarem els dies que hi ha entre ambdues
+        int compt = 0;
+        while (comparaDates(dia1, mes1, any1, dia2, mes2, any2) != 0) {
+            //Calculem data del dia següent de la data1, i l'actualitzem
+            int[] dema = diaSeguent(dia1, mes1, any1);
+            dia1 = dema[0];
+            mes1 = dema[1];
+            any1 = dema[2];
+
+            //Comptem que ha passat un dia
+            compt++;
+        }
+
+        //Retornem el número de dies entre ambdues dates
+        return compt;
+    }
+}
 
 
 

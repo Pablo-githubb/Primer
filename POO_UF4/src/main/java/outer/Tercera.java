3259aa88980b0a36a3bae4b4b9014791eb57ad68
Pablo-githubb <pablomaso@iesebre.com>
2025-03-10 14:02:44 +0100

    primer commit

--- POO_UF4/src/main/java/outer/Tercera.java
+++ POO_UF4/src/main/java/outer/Tercera.java
@@ -1,4 +1,12 @@
 package outer;
 
+import org.example.AlumnePublic;
+
 public class Tercera {
+    public static void main(String []args){
+        AlumnePublic a=new AlumnePublic();
+        //dni és protectes i només podem accedir desde classes del mateix paquet on es troba AlumnePublic
+        //System.out.println(a.dni);
+
+    }
 }
